161071496a9da876b3a484b09141f7bc837e56c2
Thomasevano <thomas-evano@hotmail.fr>
2021-06-07 23:51:29 +0200

    feat(register): Add register page

---
+++ src/pages/Register.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>S'inscrire</h1>
+  <text-field label="Email" placeholder="Email"/>
+  <text-field label="Password" placeholder="Password"/>
+  <text-field label="Confirm password" placeholder="Confirm your password"/>
+  <my-button text="Create your account" typeButton="primary"/>
+</template>
+
+<script>
+import MyButton from '../components/Button/Button.vue';
+import TextField from '../components/TextField/TextField.vue';
+
+export default {
+  name: 'Register',
+
+  components: {
+    MyButton,
+    TextField,
+  },
+}
+
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../pages/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../pages/Home.vue';
+import Register from '../pages/Register.vue';
+
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
   },
 ]
 const router = createRouter({
