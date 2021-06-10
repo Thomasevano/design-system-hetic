d70879b6825a111da78dfacf5b3c535afd7ce9b5
thomasevano <thomas-evano@hotmail.fr>
2021-06-10 23:06:43 +0200

    style(register): Styling register page

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,26 +1,30 @@
 <template>
-  <h1>S'inscrire</h1>
-  <text-field label="Email" placeholder="Email"/>
-  <text-field label="Password" placeholder="Password"/>
-  <text-field label="Confirm password" placeholder="Confirm your password"/>
-  <my-button text="Create your account" typeButton="primary"/>
+  <div class="form-page">
+    <h1 class="title">S'inscrire</h1>
+    <Form class="register-form">
+      <FormCTA message="Already got an account ?" linkText="Sign in" linkUrl="/login" />
+      <TextField label="Email" placeholder="Email" inputState="error" />
+      <TextField label="Password" placeholder="Password" />
+      <TextField label="Confirm password" placeholder="Confirm your password" />
+      <my-button text="Create your account" typeButton="primary" />
+    </Form>
+  </div>
 </template>
 
 <script>
-import MyButton from '../components/Button/Button.vue';
-import TextField from '../components/TextField/TextField.vue';
+import MyButton from "../components/Button/Button.vue";
+import TextField from "../components/TextField/TextField.vue";
+import FormCTA from "../components/FormCTA/FormCTA.vue";
+import { Form } from 'vee-validate';
 
 export default {
-  name: 'Register',
+  name: "Register",
 
   components: {
     MyButton,
     TextField,
+    FormCTA,
+    Form,
   },
-}
-
+};
 </script>
-
-<style>
-
-</style>
