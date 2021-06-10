262c19ce35562df8c11879cb229b8f8ca6aea6bd
thomasevano <thomas-evano@hotmail.fr>
2021-06-10 23:05:56 +0200

    feat(FormCTA): Add FormCTA

---
+++ src/components/FormCTA/FormCTA.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="form-cta">
+    <p class="link-message">{{ message }}</p>
+    <LinkButton :text="linkText" :linkUrl="linkUrl"/>
+  </div>
+</template>
+
+<script>
+import './formcta.scss';
+import LinkButton from '../LinkButton/LinkButton.vue';
+
+export default {
+  name: 'FormCTA',
+
+  components: {
+    LinkButton,
+  },
+
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+    linkText: {
+      type: String,
+      required: true,
+    },
+    linkUrl: {
+      type: String,
+      required: true,
+    }
+  },
+};
+</script>
