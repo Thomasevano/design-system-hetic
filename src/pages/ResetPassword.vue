55dc343df1b1696af34e86c3bd96d90b500083b4
thomasevano <thomas-evano@hotmail.fr>
2021-06-12 23:15:08 +0200

    feat(ResetPassword): Add reset password page

---
+++ src/pages/ResetPassword.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="form-page">
+    <h1 class="title">Reset Password</h1>
+    <Form class="reset-password-form">
+      <TextField label="Email" placeholder="Email" inputState="error" />
+      <TextField label="Password" placeholder="Password" />
+      <TextField label="Confirm password" placeholder="Confirm your password" />
+      <my-button text="Confirm" typeButton="primary" />
+      <my-button text="Back" typeButton="secondary" />
+    </Form>
+  </div>
+</template>
+
+<script>
+import '../styles/main.scss';
+import MyButton from "../components/Button/Button.vue";
+import TextField from "../components/TextField/TextField.vue";
+import { Form } from 'vee-validate';
+
+export default {
+  name: "ResetPassword",
+
+  components: {
+    MyButton,
+    TextField,
+    Form,
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../pages/Home.vue';
 import Register from '../pages/Register.vue';
 import Login from '../pages/Login.vue';
+import ResetPassword from '../pages/ResetPassword.vue';
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login,
+  },
+  {
+    path: '/reset',
+    name: 'ResetPassword',
+    component: ResetPassword,
   },
 ]
 const router = createRouter({
