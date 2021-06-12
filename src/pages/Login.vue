40b5aaa2da3d106964c9ddb81736637bf60021ff
thomasevano <thomas-evano@hotmail.fr>
2021-06-12 22:18:06 +0200

    feat(Login): Add Login page

---
+++ src/pages/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="form-page">
+    <h1 class="title">Se connecter</h1>
+    <Form class="login-form">
+      <FormCTA message="Not signed up ?" linkText="Register" linkUrl="/register" />
+      <TextField label="Email" placeholder="Email" inputState="error" />
+      <TextField label="Password" placeholder="Password" />
+      <LinkButton text="Reset password" linkUrl="/reset"/>
+      <my-button text="Login" typeButton="primary" />
+    </Form>
+  </div>
+</template>
+
+<script>
+import '../styles/main.scss';
+import MyButton from "../components/Button/Button.vue";
+import TextField from "../components/TextField/TextField.vue";
+import FormCTA from "../components/FormCTA/FormCTA.vue";
+import LinkButton from "../components/LinkButton/LinkButton.vue"
+import { Form } from 'vee-validate';
+
+export default {
+  name: "Login",
+
+  components: {
+    MyButton,
+    TextField,
+    FormCTA,
+    Form,
+    LinkButton,
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../pages/Home.vue';
 import Register from '../pages/Register.vue';
+import Login from '../pages/Login.vue';
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/register',
     name: 'Register',
     component: Register,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
   },
 ]
 const router = createRouter({
