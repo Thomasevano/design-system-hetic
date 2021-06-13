89c245f78dcff2c256c9a55b38494451cf4b1fc3
thomasevano <thomas-evano@hotmail.fr>
2021-06-14 00:53:55 +0200

    feat(RestPassword): Add form validation on ResetPassword page

--- src/pages/ResetPassword.vue
+++ src/pages/ResetPassword.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="form-page">
     <h1 class="title">Reset Password</h1>
-    <Form class="reset-password-form">
-      <TextField label="Email" placeholder="Email" inputState="error" />
-      <TextField label="Password" placeholder="Password" />
-      <TextField label="Confirm password" placeholder="Confirm your password" />
+    <Form class="reset-password-form" @submit="onSubmit">
+      <TextField label="Email" placeholder="Email" name="email" type="email" :inputState="emailState" :validationRules="validateEmail"/>
+      <TextField label="Password" placeholder="Password" name="password" type="password" :inputState="passwordState" :validationRules="validatePassword"/>
+      <TextField label="Confirm password" placeholder="Confirm your password" name="confirmPassword" type="password" :inputState="confirmPasswordState" :validationRules="validateConfirmPassword"/>
       <Button text="Confirm" typeButton="primary" />
       <Button text="Back" typeButton="secondary" />
     </Form>
@@ -24,6 +24,66 @@ export default {
     Button,
     TextField,
     Form,
+  },
+
+  data() {
+    let emailState;
+    let passwordState;
+    let confirmPasswordState;
+
+    return {
+      emailState,
+      passwordState,
+      confirmPasswordState,
+    }
+  },
+
+  methods: {
+    onSubmit(values) {
+      alert(values);
+    },
+    validateEmail(value) {
+      if (!value) {
+        this.emailState = 'error';
+        return `: This field is required`;
+      }
+
+      if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+        this.emailState = 'error';
+        return ': This field must be a valid email';
+      }
+
+      this.emailState = 'validated'
+      return true;
+    },
+    validatePassword(value) {
+      if (!value) {
+        this.passwordState = 'error';
+        return ': This field is required';
+      }
+
+      if(!/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/i.test(value)) {
+        this.passwordState = 'error';
+        return ': This field must be a valid password';
+      }
+
+      this.passwordState = 'validated'
+      return true;
+    },
+    validateConfirmPassword(value) {
+      if (!value) {
+        this.confirmPasswordState = 'error';
+        return ': This field is required';
+      }
+
+      if(!/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/i.test(value)) {
+        this.confirmPasswordState = 'error';
+        return ': This field must be a valid password';
+      }
+
+      this.confirmPasswordState = 'validated'
+      return true;
+    }
   },
 };
 </script>
